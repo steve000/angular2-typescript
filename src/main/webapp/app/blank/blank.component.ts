ee3a04274bfcc5b52f82bad06d13804ade01afff
linjie <linjie@cjwsjy.com.cn>
2017-04-17 17:31:36 +0800

    降雨预报 Signed-off-by: linjie <linjie@cjwsjy.com.cn>

--- src/main/webapp/app/blank/blank.component.ts
+++ src/main/webapp/app/blank/blank.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem,MenuItem } from 'primeng/primeng';
 
 @Component({
     moduleId: module.id,
     selector: 'jhi-blank',
-    templateUrl: 'blank.component.html'
+    templateUrl: 'blank.component.html',
+    styleUrls: [
+        'blank.css'
+    ]
 })
 export class BlankComponent implements OnInit {
-    constructor() {}
+    	cities: SelectItem[];
+    constructor() {
+    	this.cities = [];
+        this.cities.push({label:'重庆市及周边预报', value:{id:1, name: 'zq', code: 'zq'}});
+        this.cities.push({label:'中央气象台预报', value:{id:2, name: 'zy', code: 'zy'}});
+    }
 
     ngOnInit() {
     }
