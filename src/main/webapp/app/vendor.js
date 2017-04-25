795f03cf6ddb37c761b15c27b5ee289bd595edfd
angelscat <angelscat@vip.qq.com>
2017-04-25 11:29:34 +0800

    delete .js and .js.map

--- src/main/webapp/app/vendor.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/* after changing this file run 'yarn install' or 'yarn run webpack:build' */
-/* tslint:disable */
-require('../content/css/vendor.css');
-//# sourceMappingURL=vendor.js.map
